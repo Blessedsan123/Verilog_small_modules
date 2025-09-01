2f4997ef79032e8ac3dddfaee30e4c01aac70a8e
Sandeep Panigrahi <156462298+Blessedsan123@users.noreply.github.com>
2025-09-01 15:32:07 +0530

    Update test_hamming_code_corrector.v

--- Hamming_code/test_hamming_code_corrector.v
+++ Hamming_code/test_hamming_code_corrector.v
@@ -1,3 +1,5 @@
+// Code your testbench here
+// or browse Examples
 module test();
   reg [4:1] data_in;
   reg parity_type;
@@ -5,6 +7,8 @@ module test();
   wire [4:1] data_out;
   wire error_d;
   wire [3:1]parity_out;
+  wire [7:1] corrected_code;
+  wire [2:0] error_pos;
   
   hamming_top ham_top (
     .data_in(data_in),
@@ -12,7 +16,9 @@ module test();
     .code_out(code_out),
     .data_out(data_out),
     .error_d(error_d),
-    .parity_out(parity_out)
+    .parity_out(parity_out),
+    .corrected_code(corrected_code),
+    .error_pos(error_pos)
   );
   
   initial
@@ -24,14 +30,14 @@ module test();
   
   initial
     begin
-      $monitor("data_in : %b || data_out : %b || code_out : %b || parity_out : %b || error_d : %b",data_in,data_out,code_out,parity_out,error_d);
+      $monitor("data_in : %b || data_out : %b || code_out : %b || corrected_code : %b || parity_out : %b || error_d : %b || error_pos : %b ",data_in,data_out,code_out,corrected_code,parity_out,error_d,error_pos);
       data_in = 4'b0101;
       parity_type = 0;
       #10;
       data_in = 4'b1101; 
       parity_type = 0;
       #10;
-      data_in = 4'b0111;
+      data_in = 4'b0001;
       parity_type = 0;
       #10;
       data_in = 4'b0000;
